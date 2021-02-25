3a56865a69c7166ccd4c1d88d03182f3c5b49afa
Priyanshu Shukla <priyanshu.shukla_cs18@gla.ac.in>
2021-02-25 22:52:33 +0530

    Project completed

--- Calculator/Calculatorapp/views.py
+++ Calculator/Calculatorapp/views.py
@@ -4,3 +4,23 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+
+def submitquery(request):
+    q = request.GET['query']
+    try:
+        ans=eval(q)
+        mydict={
+            "q" :q,
+            "ans": ans,
+            "error": False,
+            "result":True
+        }
+        return render(request,'index.html',context=mydict)
+    except:
+        mydict={
+            "error":True,
+            "result":False
+
+        }
+        return render(request,'index.html',context=mydict)
+
